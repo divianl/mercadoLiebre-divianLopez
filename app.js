d4ae945025530a9cd884fe6f71d4558f1f030b18
divianl <divianlopezo@gmail.com>
2023-05-08 20:11:14 -0500

    correcion

--- app.js
+++ app.js
@@ -9,8 +9,16 @@ app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, 'views/home.html'));
 })
 
+
+//Levatar el servidor
+const port = process.env.PORT || 3001;
+app.listen(port, ()=>console.log('Servidor corriendo en el puerto $(port)'));
+
+
+/*
 app.listen(3004, ()=>{
     console.log("Servidor corriendo en puerto 3004");
 })
+*/
 
 
