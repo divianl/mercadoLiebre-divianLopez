0f380e02636b5a0afc7555ff1740b874fe619fed
divianl <divianlopezo@gmail.com>
2023-05-09 09:34:43 -0500

    actualizacion

--- app.js
+++ app.js
@@ -11,14 +11,10 @@ app.get('/', (req,res)=>{
 
 
 //Levatar el servidor
-const port = process.env.PORT || 3001;
-app.listen(port, ()=>console.log('Servidor corriendo en el puerto $(port)'));
+/*const port = process.env.PORT || 3001;
+app.listen(port, ()=>console.log('Servidor corriendo en el puerto $(port)'));   */
 
 
-/*
 app.listen(3004, ()=>{
     console.log("Servidor corriendo en puerto 3004");
 })
-*/
-
-
