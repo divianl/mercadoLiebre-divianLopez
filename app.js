67a81a5d2231255825e87e5920bde1e43105058f
divianl <divianlopezo@gmail.com>
2023-05-10 08:57:43 -0500

    avance

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, 'views/home.html'));
 })
 
+app.get('/login', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'views/login.html'));
+})
+
+app.get('/register', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'views/register.html'));
+})
+
 
 //Levatar el servidor
 /*const port = process.env.PORT || 3001;
